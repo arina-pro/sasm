88e578a2eb0043d25685d050ca6ce258fbbb95ef
prostako <arina.prostakova@eurecom.fr>
2023-06-21 00:35:32 +0200

    add files

--- timed_collatz.py
+++ timed_collatz.py
@@ -1,17 +1,17 @@
 import time
 
 def collatz_rec(n, l=1):
-    t_start = time.clock()
+    
     if n == 1:
-        t_end = time.clock()
-        return l, t_end - t_start
+        
+        return l#, t_end - t_start
     elif n % 2 == 0:
         return collatz_rec(n / 2, l + 1)
     else:
         return collatz_rec(3 * n + 1, l + 1)
 
 def collatz_nonrec(n):
-    t_start = time.clock()
+    
     l = 1
     while n != 1:
         l += 1
@@ -19,8 +19,16 @@ def collatz_nonrec(n):
             n = n / 2
         else:
             n = 3 * n + 1
-    t_end = time.clock()
-    return l, t_end - t_start
-
-print(collatz_rec(1234))
-print(collatz_nonrec(1234))
+    
+    return l#, t_end - t_start
+#t_start = time.clock()
+#max_l = max([collatz_rec(i) for i in range(1, 1000000)])
+#t_end = time.clock()
+#timed_rec = t_end - t_start
+#t_start = time.clock()
+#max_l = max([collatz_nonrec(i) for i in range(1, 1000000)])
+s = sum([collatz_nonrec(i) for i in range(1, 11)])
+#t_end = time.clock()
+#timed_nonrec = t_end - t_start
+print(s)
+#print(timed_rec - timed_nonrec)
